f81d69bc87b014d0a4e547af8a3b120c70910ab7
Cameron Brown <me@cbrxyz.com>
2023-11-04 23:38:14 -0400

    Fix two User declarations in gh types

--- src/github_types.py
+++ src/github_types.py
@@ -319,27 +319,6 @@ class Commit(TypedDict):
     comment_count: int
 
 
-class User(TypedDict):
-    login: str
-    id: int
-    node_id: str
-    avatar_url: str
-    gravatar_id: str
-    url: str
-    html_url: str
-    followers_url: str
-    following_url: str
-    gists_url: str
-    starred_url: str
-    subscriptions_url: str
-    organizations_url: str
-    repos_url: str
-    events_url: str
-    received_events_url: str
-    type: str
-    site_admin: bool
-
-
 class ParentCommit(TypedDict):
     url: str
     html_url: str
