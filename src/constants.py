f4f528243b2d0d9ae2982bfeba783c45f29da40a
Cameron Brown <me@cbrxyz.com>
2024-04-28 13:44:58 -0400

    Update semesters to end on 4/28

--- src/constants.py
+++ src/constants.py
@@ -6,7 +6,7 @@ from enum import auto
 
 SEMESTERS = [
     (datetime.date(2023, 8, 23), datetime.date(2023, 12, 6)),
-    (datetime.date(2024, 1, 8), datetime.date(2024, 4, 24)),
+    (datetime.date(2024, 1, 8), datetime.date(2024, 4, 28)),
     (datetime.date(2024, 5, 13), datetime.date(2024, 8, 9)),
 ]
 
