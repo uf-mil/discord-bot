116f0102edf1bd1ad7e545873e8b0dca3ae5946f
cameron brown <52760912+cbrxyz@users.noreply.github.com>
2024-07-16 01:17:42 -0400

    src/webhooks.py: Add more GitHub webhooks (membership, comments, repository) (#59)

--- src/webhook_server.py
+++ src/webhook_server.py
@@ -12,6 +12,7 @@ async def main():
     # Get headers
     data = await request.get_json()
     event_type = request.headers.get("X-GitHub-Event")
+    print(f"Received event of type {event_type}.")
     if event_type == "ping":
         print("I was pinged!")
     elif event_type == "push":
@@ -32,6 +33,22 @@ async def main():
         await ipc.request("pull_request_closed", github_data=data)
     elif event_type == "pull_request_review" and data["action"] == "submitted":
         await ipc.request("pull_request_review_submitted", github_data=data)
+    elif event_type == "commit_comment":
+        await ipc.request("commit_comment", github_data=data)
+    elif event_type == "issue_comment" and data["action"] == "created":
+        await ipc.request("issue_comment_created", github_data=data)
+    elif event_type == "issues" and data["action"] == "assigned":
+        await ipc.request("issues_assigned", github_data=data)
+    elif event_type == "issues" and data["action"] == "unassigned":
+        await ipc.request("issues_unassigned", github_data=data)
+    elif event_type == "membership" and data["action"] == "added":
+        await ipc.request("membership_added", github_data=data)
+    elif event_type == "public":
+        await ipc.request("public", github_data=data)
+    elif event_type == "repository" and data["action"] == "created":
+        await ipc.request("repository_created", github_data=data)
+    elif event_type == "repository" and data["action"] == "deleted":
+        await ipc.request("repository_deleted", github_data=data)
     return {"succeeded": True}
 
 
